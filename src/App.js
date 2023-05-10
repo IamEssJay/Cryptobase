cd61248dbec7cd0638c521999ca90d5f71a4c7b2
IamEssJay <emmasj2019@gmail.com>
2023-05-10 11:51:13 +0100

    Hmmm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { ThemeProvider } from './context/ThemeContext';
+import Home from './Pages/Home';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import accounts from './Pages/accounts';
 
 function App() {
   return <ThemeProvider>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
   </ThemeProvider>
 }
 
