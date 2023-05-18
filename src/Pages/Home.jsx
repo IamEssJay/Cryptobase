b967bf8367de9f6641dbf959166f88e85408dced
IamEssJay <emmasj2019@gmail.com>
2023-05-18 20:24:57 +0100

    Trending coins

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Search from '../components/Search'
+import Trending from '../components/Trending'
 
 const Home = ({coins}) => {
   return (
     <div>
       <Search coins={coins}/>
+      <Trending/>
     </div>
   )
 }
